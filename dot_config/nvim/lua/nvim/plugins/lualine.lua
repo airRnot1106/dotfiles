b53b50e82eca6e1e033ce1fc345b734efc400a1b
airRnot1106 <yngw.rintaro@gmail.com>
2024-05-18 10:58:06 +0900

    fix: fix dependencies repo name

--- dot_config/nvim/lua/nvim/plugins/lualine.lua
+++ dot_config/nvim/lua/nvim/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-web-devicons", opt = true },
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
     event = { "BufNewFile", "BufRead" },
     options = { theme = "gruvbox-material" },
     config = function()
