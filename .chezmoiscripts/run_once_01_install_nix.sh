3289a11a78759610d5f7576d30a570e8e8cd169d
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-17 23:37:48 +0900

    fix: fix commands

--- .chezmoiscripts/run_once_01_install_nix.sh
+++ .chezmoiscripts/run_once_01_install_nix.sh
@@ -7,6 +7,7 @@ echo "Installing Nix..."
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
 echo "Installation completed.
-Next Steps: Execute the following command.
+Next Steps: Execute the following commands.
+chezmoi cd
 nix run nixpkgs#home-manager -- switch --flake .#airrnot
 "
