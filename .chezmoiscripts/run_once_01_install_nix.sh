50f4e7a2653db29433b95295d28bb6ef78f690c6
airRnot <62370527+airRnot1106@users.noreply.github.com>
2024-09-18 23:23:22 +0900

    Update run_once_01_install_nix.sh

--- .chezmoiscripts/run_once_01_install_nix.sh
+++ .chezmoiscripts/run_once_01_install_nix.sh
@@ -6,12 +6,12 @@ echo "Installing Nix..."
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 sudo chmod 666 /dev/tty
-if [ -f "$HOME/.bashrc" ]; then
-    rm "$HOME/.bashrc"
-fi
-if [ -f "$HOME/.profile" ]; then
-    rm "$HOME/.profile"
-fi
+#if [ -f "$HOME/.bashrc" ]; then
+#    rm "$HOME/.bashrc"
+#fi
+#if [ -f "$HOME/.profile" ]; then
+#    rm "$HOME/.profile"
+#fi
 
 echo "Installation completed.
 Next Steps: Execute the following commands.
