5ae60e9036d1d185f54ddebc0a4dfbf7c1261db3
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-21 21:58:11 +0900

    Update home/commons/modules/editor/neovim.nix

--- home/commons/modules/editor/neovim.nix
+++ home/commons/modules/editor/neovim.nix
@@ -2,9 +2,7 @@
 {
   programs.neovim = {
     enable = true;
-    # package = pkgs.neovim.overrideAttrs (old: {
-    #   version = "0.10.1";
-    # }); # inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+    # package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
