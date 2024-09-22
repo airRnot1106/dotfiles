17cc7fddcf9d59f6fa5ea807b4fc054476d12413
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-22 22:02:12 +0900

    Add .chezmoiroot Update .chezmoiscripts/02_create_script_set_env_variables.sh Update .chezmoiscripts/zz_echo_next_steps.sh

--- .chezmoiscripts/run_once_zz_echo_next_steps.sh
+++ .chezmoiscripts/run_once_zz_echo_next_steps.sh
@@ -4,6 +4,7 @@ set -eu
 
 echo "Next Steps: Execute the following commands.
 ------------------------------------------
+source ./set_env_variables.sh
 $HOME/.local/bin/chezmoi cd
 nix run
 ------------------------------------------
