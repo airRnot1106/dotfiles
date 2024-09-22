3a1c207d01c86f0bbf377b277ac221c53ff699ce
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-22 17:04:51 +0900

    Update nix-darwin/system/dock.nix

--- nix-darwin/system/dock.nix
+++ nix-darwin/system/dock.nix
@@ -15,7 +15,7 @@
         persistent-apps = [
           "/Applications/Arc.app/"
           "/nix/store/2790rj9mndm2h6qysik0rwasnqfzab32-vscode-1.92.0/Applications/Visual Studio Code.app"
-          "/Applications/Warp.app/"
+          "/nix/store/hc0l6kmzn1vqkqx7g0s4ggisd5mcb85k-warp-terminal-0.2024.07.16.08.02.stable_03/Applications/Warp.app"
           "/System/Applications/System\ Settings.app/"
         ];
         wvous-bl-corner = 5;
