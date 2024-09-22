ab98e759d758e92c5a5f53d5dd7b3de4a8cea835
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-22 18:46:33 +0900

    Update flake.lock Update nix-darwin/system/dock.nix

--- nix-darwin/system/dock.nix
+++ nix-darwin/system/dock.nix
@@ -14,11 +14,11 @@
         mru-spaces = false;
         persistent-apps = [
           # "/nix/store/v8v6agaa9q0bxvgv5348885l5vv534gm-arc-browser-1.55.0-52417/Applications/Arc.app"
-          "$HOME/.nix-profile/Applications/Arc.app"
+          "~/.nix-profile/Applications/Arc.app"
           # "/nix/store/2790rj9mndm2h6qysik0rwasnqfzab32-vscode-1.92.0/Applications/Visual Studio Code.app"
-          "$HOME/.nix-profile/Applications/Visual Studio Code.app"
+          "~/.nix-profile/Applications/Visual Studio Code.app"
           # "/nix/store/hc0l6kmzn1vqkqx7g0s4ggisd5mcb85k-warp-terminal-0.2024.07.16.08.02.stable_03/Applications/Warp.app"
-          "$HOME/.nix-profile/Applications/Warp.app"
+          "~/.nix-profile/Applications/Warp.app"
           "/System/Applications/System\ Settings.app/"
         ];
         wvous-bl-corner = 5;
