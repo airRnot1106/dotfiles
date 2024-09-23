e00ae311767c000cc6d0e6b166f0eaf79d33c422
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-23 14:09:00 +0900

    Update nixos/boot/default.nix

---
+++ nixos/boot/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        editor = false;
+      };
+      efi.canTouchEfiVariables = true;
+      timeout = 3;
+    };
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    tmp = {
+      cleanOnBoot = true;
+      useTmpfs = false;
+    };
+  };
+}
