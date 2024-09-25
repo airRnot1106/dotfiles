60a229667bfe86153c60ecfac5d1193736ec0887
airRnot1106 <yngw.rintaro@gmail.com>
2024-09-25 09:20:47 +0900

    Add home/commons/default.nix Update home/darwin/default.nix Update home/linux/default.nix

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,50 +1,16 @@
-{ inputs, ... }:
-# { pkgs, inputs, ... }:
+{ ... }:
 let
   inherit (import ../options.nix)
     username
-    gitUsername
-    gitEmail
-    openaiKey
     ;
 in
 {
-  nixpkgs = {
-    overlays = [
-      inputs.neovim-nightly-overlay.overlays.default
-      inputs.nix-vscode-extensions.overlays.default
-    ];
-    # overlays = [ (import ../overlays/neovim.nix { inherit pkgs; }) ];
-    config = {
-      allowUnfree = true;
-    };
-  };
-
   home = {
-    username = username;
     homeDirectory = "/Users/${username}";
-
-    sessionPath = [ "$HOME/.local/bin" ];
-    sessionVariables = {
-      GIT_USERNAME = gitUsername;
-      GIT_EMAIL = gitEmail;
-      OPENAI_KEY = openaiKey;
-    };
-
-    shellAliases = {
-      ccd = "chezmoi cd";
-      ced = "chezmoi edit";
-    };
-
-    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    stateVersion = "24.05";
-
   };
 
   imports = [
-    ../commons/modules
+    ../commons
     ./modules
   ];
-
-  programs.home-manager.enable = true;
 }
