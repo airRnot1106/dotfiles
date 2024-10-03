df405363988e1f0c07046449546598098d47a894
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-04 01:28:27 +0900

    Update home/commons/modules/dev/lsps/default.nix Remove home/commons/modules/dev/lsps/mdx-language-server/default.nix Remove home/commons/modules/dev/lsps/mdx-language-server/node-env.nix Remove home/commons/modules/dev/lsps/mdx-language-server/node-packages.nix

--- home/commons/modules/dev/lsps/default.nix
+++ home/commons/modules/dev/lsps/default.nix
@@ -4,7 +4,6 @@
     ./bash-language-server.nix
     ./efm-langserver.nix
     ./lua-language-server.nix
-    ./mdx-language-server
     ./nil.nix
     ./typescript-language-server.nix
     ./tailwindcss-language-server.nix
