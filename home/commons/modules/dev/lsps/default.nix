80aa9bb052937a483781b11a1eaf0cafb65c8bd9
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-04 01:02:25 +0900

    Update home/commons/modules/dev/lsps/default.nix Add home/commons/modules/dev/lsps/mdx-language-server/default.nix Add home/commons/modules/dev/lsps/mdx-language-server/node-env.nix Add home/commons/modules/dev/lsps/mdx-language-server/node-packages.nix

--- home/commons/modules/dev/lsps/default.nix
+++ home/commons/modules/dev/lsps/default.nix
@@ -4,6 +4,7 @@
     ./bash-language-server.nix
     ./efm-langserver.nix
     ./lua-language-server.nix
+    ./mdx-language-server
     ./nil.nix
     ./typescript-language-server.nix
     ./tailwindcss-language-server.nix
