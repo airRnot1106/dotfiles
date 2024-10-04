6548274373200a418fdd4d6b6752ca0d13686294
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-04 12:18:09 +0900

    Update flake.lock Update home/commons/modules/dev/lsps/default.nix

--- home/commons/modules/dev/lsps/default.nix
+++ home/commons/modules/dev/lsps/default.nix
@@ -4,7 +4,6 @@
     ./bash-language-server.nix
     ./efm-langserver.nix
     ./lua-language-server.nix
-    ./nil.nix
     ./typescript-language-server.nix
     ./tailwindcss-language-server.nix
   ];
