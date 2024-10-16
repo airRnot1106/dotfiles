c337b5358245a92e1c5d797e5ffdd4089eec64a7
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 18:18:45 +0900

    Update home/commons/modules/shell/tmux.nix

--- home/commons/modules/shell/tmux.nix
+++ home/commons/modules/shell/tmux.nix
@@ -2,7 +2,6 @@
 {
   programs.tmux = {
     enable = true;
-    # see: https://github.com/nix-community/home-manager/issues/5952
     package = pkgs.tmux.overrideAttrs (old: rec {
       version = "3.5";
       src = pkgs.fetchFromGitHub {
@@ -19,16 +18,16 @@
     newSession = true;
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = rose-pine;
-        extraConfig = ''
-          set -g @rose_pine_variant 'moon'
-          set -g @rose_pine_date_time '%Y-%m-%d %H:%M'
-          set -g @rose_pine_disable_active_window_menu 'on'
-          set -g @rose_pine_bar_bg_disable 'on'
-          set -g @rose_pine_bar_bg_disabled_color_option 'default'
-        '';
-      }
+      # {
+      #   plugin = rose-pine;
+      #   extraConfig = ''
+      #     set -g @rose_pine_variant 'moon'
+      #     set -g @rose_pine_date_time '%Y-%m-%d %H:%M'
+      #     set -g @rose_pine_disable_active_window_menu 'on'
+      #     set -g @rose_pine_bar_bg_disable 'on'
+      #     set -g @rose_pine_bar_bg_disabled_color_option 'default'
+      #   '';
+      # }
     ];
     terminal = "tmux-256color";
     extraConfig = ''
