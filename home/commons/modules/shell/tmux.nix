bff88ebe3fbef7b5f399757fd38a27b064c6aab6
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 18:20:55 +0900

    Update home/commons/modules/shell/tmux.nix

--- home/commons/modules/shell/tmux.nix
+++ home/commons/modules/shell/tmux.nix
@@ -11,12 +11,12 @@
         hash = "sha256-8CRZj7UyBhuB5QO27Y+tHG62S/eGxPOHWrwvh1aBqq0=";
       };
     });
-    aggressiveResize = true;
-    clock24 = true;
-    escapeTime = 0;
-    historyLimit = 30000;
-    newSession = true;
-    keyMode = "vi";
+    # aggressiveResize = true;
+    # clock24 = true;
+    # escapeTime = 0;
+    # historyLimit = 30000;
+    # newSession = true;
+    # keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
       # {
       #   plugin = rose-pine;
@@ -29,12 +29,12 @@
       #   '';
       # }
     ];
-    terminal = "tmux-256color";
-    extraConfig = ''
-      # update the env when attaching to an existing session
-      set -g update-environment -r
-
-      set -ag terminal-overrides ",alacritty*:Tc,foot*:Tc,xterm-kitty*:Tc,xterm-256color:Tc"
-    '';
+    # terminal = "tmux-256color";
+    # extraConfig = ''
+    #   # update the env when attaching to an existing session
+    #   set -g update-environment -r
+    #
+    #   set -ag terminal-overrides ",alacritty*:Tc,foot*:Tc,xterm-kitty*:Tc,xterm-256color:Tc"
+    # '';
   };
 }
