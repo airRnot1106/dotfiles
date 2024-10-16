d107ee463bea2de25008f82170bba2339443ea0d
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 18:14:11 +0900

    Update home/commons/modules/shell/tmux.nix

--- home/commons/modules/shell/tmux.nix
+++ home/commons/modules/shell/tmux.nix
@@ -27,19 +27,6 @@
           set -g @rose_pine_disable_active_window_menu 'on'
           set -g @rose_pine_bar_bg_disable 'on'
           set -g @rose_pine_bar_bg_disabled_color_option 'default'
-
-          set -g @rose_pine_left_separator ' > ' # The strings to use as separators are 1-space padded
-          set -g @rose_pine_right_separator ' < ' # Accepts both normal chars & nerdfont icons
-          set -g @rose_pine_field_separator ' | ' # Again, 1-space padding, it updates with prefix + I
-          set -g @rose_pine_window_separator ' - ' # Replaces the default `:` between the window number and name
-
-          set -g @rose_pine_session_icon '' # Changes the default icon to the left of the session name
-          set -g @rose_pine_current_window_icon '' # Changes the default icon to the left of the active window name
-          set -g @rose_pine_folder_icon '' # Changes the default icon to the left of the current directory folder
-          set -g @rose_pine_username_icon '' # Changes the default icon to the right of the hostname
-          set -g @rose_pine_hostname_icon '󰒋' # Changes the default icon to the right of the hostname
-          set -g @rose_pine_date_time_icon '󰃰' # Changes the default icon to the right of the date module
-          set -g @rose_pine_window_status_separator "  " # Changes the default icon that appears between window names
         '';
       }
     ];
