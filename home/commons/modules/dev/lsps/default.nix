5772df002ba18883b71118325f59284b5fc81d37
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 09:28:05 +0900

    Update home/commons/modules/dev/lsps/default.nix Add home/commons/modules/shell/alacritty.nix

--- home/commons/modules/dev/lsps/default.nix
+++ home/commons/modules/dev/lsps/default.nix
@@ -5,6 +5,7 @@
     ./bash-language-server.nix
     ./efm-langserver.nix
     ./lua-language-server.nix
+    ./nil.nix
     ./typescript-language-server.nix
     ./tailwindcss-language-server.nix
   ];
