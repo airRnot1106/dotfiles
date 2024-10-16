b916232160e5d68c311373d170df014fa8951147
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 23:21:55 +0900

    Update home/commons/modules/shell/configs/zellij/config.kdl Update home/commons/modules/shell/zellij.nix

--- home/commons/modules/shell/zellij.nix
+++ home/commons/modules/shell/zellij.nix
@@ -2,36 +2,12 @@
 {
   programs.zellij = {
     enable = true;
-    settings = {
-      theme = "rose-pine-moon";
-      themes = {
-        rose-pine-moon = {
-          bg = "#44415a";
-          fg = "#e0def4";
-          red = "#eb6f92";
-          green = "#3e8fb0";
-          blue = "#9ccfd8";
-          yellow = "#f6c177";
-          magenta = "#c4a7e7";
-          orange = "#fe640b";
-          cyan = "#ea9a97";
-          black = "#393552";
-          white = "#e0def4";
-        };
-      };
-      pane_frames = false;
-      keybinds = {
-        "shared_except \"normal\"" = {
-          "bind \"q\"" = {
-            SwitchToMode = "normal";
-          };
-        };
-        "normal clear-defaults=true" = {
-          "bind \"Ctrl b\"" = {
-            SwitchToMode = "tmux";
-          };
-        };
-      };
+  };
+
+  home.file = {
+    zellij = {
+      source = ./configs/zellij;
+      target = ".config/zellij";
     };
   };
 }
