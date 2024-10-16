9e160378dbec044400157e81e6624a86c5ca919f
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 18:25:20 +0900

    Update home/commons/modules/shell/tmux.nix Add tmux-client-63737.log

--- home/commons/modules/shell/tmux.nix
+++ home/commons/modules/shell/tmux.nix
@@ -2,39 +2,5 @@
 {
   programs.tmux = {
     enable = true;
-    # package = pkgs.tmux.overrideAttrs (old: rec {
-    #   version = "3.5";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "tmux";
-    #     repo = "tmux";
-    #     rev = version;
-    #     hash = "sha256-8CRZj7UyBhuB5QO27Y+tHG62S/eGxPOHWrwvh1aBqq0=";
-    #   };
-    # });
-    # aggressiveResize = true;
-    # clock24 = true;
-    # escapeTime = 0;
-    # historyLimit = 30000;
-    # newSession = true;
-    # keyMode = "vi";
-    plugins = with pkgs.tmuxPlugins; [
-      # {
-      #   plugin = rose-pine;
-      #   extraConfig = ''
-      #     set -g @rose_pine_variant 'moon'
-      #     set -g @rose_pine_date_time '%Y-%m-%d %H:%M'
-      #     set -g @rose_pine_disable_active_window_menu 'on'
-      #     set -g @rose_pine_bar_bg_disable 'on'
-      #     set -g @rose_pine_bar_bg_disabled_color_option 'default'
-      #   '';
-      # }
-    ];
-    # terminal = "tmux-256color";
-    # extraConfig = ''
-    #   # update the env when attaching to an existing session
-    #   set -g update-environment -r
-    #
-    #   set -ag terminal-overrides ",alacritty*:Tc,foot*:Tc,xterm-kitty*:Tc,xterm-256color:Tc"
-    # '';
   };
 }
