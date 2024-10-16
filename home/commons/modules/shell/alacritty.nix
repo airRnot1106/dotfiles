b058b051740a5f2804f9c8c609113823b86f7f4a
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-16 10:34:40 +0900

    Update home/commons/modules/shell/alacritty.nix

--- home/commons/modules/shell/alacritty.nix
+++ home/commons/modules/shell/alacritty.nix
@@ -2,19 +2,17 @@
 {
   programs.alacritty = {
     enable = true;
-    settings = ''
-      {
-        font = {
-          normal = {
-            family = "PlemolJP Console NF";
-          };
-          size = 15.0;
-          offset = {
-            x = 1;
-            y = 1;
-          };
+    settings = {
+      font = {
+        normal = {
+          family = "PlemolJP Console NF";
         };
-      }
-    '';
+        size = 15.0;
+        offset = {
+          x = 1;
+          y = 1;
+        };
+      };
+    };
   };
 }
