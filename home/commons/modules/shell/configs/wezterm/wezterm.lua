6953fb054b9fae0c3cc96db9fffc4e4fca00d34c
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-17 21:41:16 +0900

    Update home/commons/modules/shell/configs/wezterm/wezterm.lua

--- home/commons/modules/shell/configs/wezterm/wezterm.lua
+++ home/commons/modules/shell/configs/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
-local wezterm = require("wezterm")
-
 local config = {}
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
+
+config.font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 return config
