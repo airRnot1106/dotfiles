9c48d19ac561a0a6e09477f22deb75d61d82d7b7
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-17 23:49:26 +0900

    Update home/commons/modules/shell/configs/wezterm/configs/keybinds.lua

--- home/commons/modules/shell/configs/wezterm/configs/keybinds.lua
+++ home/commons/modules/shell/configs/wezterm/configs/keybinds.lua
@@ -1,7 +1,12 @@
 local M = {}
+local wezterm = require("wezterm")
 
 function M.setup(config)
 	config.disable_default_key_bindings = true
+
+	config.keys = {
+		{ key = "q", mods = "CMD", action = wezterm.action.QuitApplication },
+	}
 
 	return config
 end
