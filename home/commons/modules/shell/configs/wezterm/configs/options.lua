a10f357ccc3a390d67dd9e15787b6b9ceb89fb8d
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-17 22:53:04 +0900

    Update home/commons/modules/shell/configs/wezterm/configs/font.lua Update home/commons/modules/shell/configs/wezterm/configs/options.lua

--- home/commons/modules/shell/configs/wezterm/configs/options.lua
+++ home/commons/modules/shell/configs/wezterm/configs/options.lua
@@ -5,6 +5,7 @@ function M.setup(config)
 	config.use_ime = true
 
 	config.window_decorations = "RESIZE"
+	config.show_tabs_in_tab_bar = false
 	config.hide_tab_bar_if_only_one_tab = true
 	config.use_fancy_tab_bar = false
 	config.show_new_tab_button_in_tab_bar = false
