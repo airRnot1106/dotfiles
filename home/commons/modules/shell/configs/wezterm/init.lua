34664e366a2e1cdc31fe0976df69dfffbd70e01b
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-19 17:39:38 +0900

    Update home/commons/modules/shell/configs/wezterm/init.lua

--- home/commons/modules/shell/configs/wezterm/init.lua
+++ home/commons/modules/shell/configs/wezterm/init.lua
@@ -13,9 +13,12 @@ config = require("configs.keybinds").setup(config)
 
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
+	local screen = wezterm.gui.screens().active
+	local width = screen.width * 0.85
+	local height = screen.height * 0.85
 	local tab, pane, window = mux.spawn_window(cmd or { width = 158, height = 56 })
 	window:gui_window():set_position(0, 0)
-	window:set_inner_size(800, 1020)
+	window:set_inner_size(width, height)
 end)
 
 return config
