1c3c0d0e2c9a4939b07e09ecc970b96e233b5f0f
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-22 22:47:55 +0900

    Update home/commons/modules/git/lazygit.nix

--- home/commons/modules/git/lazygit.nix
+++ home/commons/modules/git/lazygit.nix
@@ -14,6 +14,16 @@
         allBranchesLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' --";
         branchLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' {{branchName}} --";
       };
+      customCommands = [
+        {
+          key = "a";
+          description = "aicommits";
+          command = "aicommits";
+          context = "files";
+          subprocess = true;
+          showOutput = true;
+        }
+      ];
     };
   };
 }
