1d45235bc4972592122ed70ef2565c0f42c1c87e
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-05-16 23:15:10 +0900

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "airRnot's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            direnv
+            git
+          ];
+        };
+      }
+    );
+}
