e433c31090643fff27e42bc3ab2e0971910d37b9
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-16 23:43:07 +0900

    feat: add treefmt

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,19 +15,32 @@
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
       ...
     }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        treefmt = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+          };
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             direnv
             git
+            treefmt
           ];
+        };
+        formatter = treefmt;
+        checks.formatting = treefmt-nix.lib.mkCheck pkgs {
+          inherit treefmt;
+          projectRoot = self;
         };
       }
     );
