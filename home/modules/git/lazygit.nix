26e5c798b7f14cefedb7d2b2225ddb93451b1108
airRnot1106 <yngw.rintaro@gmail.com>
2025-06-05 06:53:15 +0900

    chore: migrate lazygit settings

--- home/modules/git/lazygit.nix
+++ home/modules/git/lazygit.nix
@@ -11,7 +11,6 @@
           colorArg = "always";
           pager = "delta --dark --paging=never";
         };
-        allBranchesLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' --";
         branchLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' {{branchName}} --";
       };
       customCommands = [
@@ -20,8 +19,7 @@
           description = "aicommits";
           command = "aicommits";
           context = "files";
-          subprocess = true;
-          showOutput = true;
+          output = "terminal";
         }
       ];
     };
