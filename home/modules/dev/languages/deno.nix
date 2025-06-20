1804e7df04f69dc5d884181c581b53eb23e1ad88
airRnot1106 <yngw.rintaro@gmail.com>
2025-06-20 09:36:14 +0900

    refactor: change to overlay

--- home/modules/dev/languages/deno.nix
+++ home/modules/dev/languages/deno.nix
@@ -1,22 +1,4 @@
 { pkgs, ... }:
-let
-  deno-2-3-4 = pkgs.deno.overrideAttrs (oldAttrs: rec {
-    version = "2.3.4";
-    src = pkgs.fetchFromGitHub {
-      owner = "denoland";
-      repo = "deno";
-      rev = "v${version}";
-      hash = "sha256-oGD4Qz4i2mdyyxRFZYxtT8DfX0DaFcmW9iai2kbrQPE=";
-    };
-    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-      inherit src;
-      name = "deno-${version}";
-      hash = "sha256-NigdetbEAgaH+CqiqJUMYUkVIOsK+zJsy4qjLVfVWSY=";
-    };
-    doInstallCheck = false;
-    doCheck = false;
-  });
-in
 {
-  home.packages = [ deno-2-3-4 ];
+  home.packages = with pkgs; [ deno ];
 }
