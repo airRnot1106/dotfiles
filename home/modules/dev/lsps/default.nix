ce80eae62056c6649f0d8de1b59e7bae53e6a6eb
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-10-09 22:41:10 +0900

    feat: add python-lsp-server & mypy

--- home/modules/dev/lsps/default.nix
+++ home/modules/dev/lsps/default.nix
@@ -5,7 +5,9 @@
     ./efm-langserver.nix
     ./lua-language-server.nix
     ./mdx-language-server.nix
+    ./mypy.nix
     ./nil.nix
+    ./python-lsp-server.nix
     ./tailwindcss-language-server.nix
     ./typescript-language-server.nix
     ./vscode-langservers-extracted.nix
