4679731ce110b17e8e38e06f9797ce85c049d46b
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-25 15:46:39 +0900

    chore: update git settings

--- home/modules/git/lazygit.nix
+++ home/modules/git/lazygit.nix
@@ -11,16 +11,8 @@
           pager = "delta --dark --paging=never";
         };
         branchLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' {{branchName}} --";
+        overrideGpg = true;
       };
-      customCommands = [
-        {
-          key = "<c-a>";
-          description = "aicommits";
-          command = "aicommits";
-          context = "files";
-          output = "terminal";
-        }
-      ];
     };
   };
 }
