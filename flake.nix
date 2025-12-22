8c1c20ea50a66bbfc986c5f3a9b6cd6ca518dac2
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-12-22 13:54:04 +0900

    chore: make the cachix cache available locally

--- flake.nix
+++ flake.nix
@@ -42,6 +42,17 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://airrnot.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "airrnot.cachix.org-1:w3kgp+iIESfezlrppODxo9Ahm1QLkCJXDhEuxYfUV7k="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     {
       flake-parts,
--- nix-darwin/nix/default.nix
+++ nix-darwin/nix/default.nix
@@ -1,17 +1,24 @@
-let
-  inherit (import ../../profile.nix)
-    username
-    ;
-in
+{ pkgs, lib, ... }:
 {
   nix = {
-    extraOptions = ''
-      trusted-users = root ${username}
-    '';
     optimise.automatic = true;
     settings = {
       experimental-features = "nix-command flakes";
       max-jobs = 8;
+
+      extra-substituters = [
+        "https://airrnot.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "airrnot.cachix.org-1:w3kgp+iIESfezlrppODxo9Ahm1QLkCJXDhEuxYfUV7k="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ]
+      ++ lib.optional pkgs.stdenv.isDarwin "@admin";
     };
   };
 }
