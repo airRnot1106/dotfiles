f32b2afc8df818a7467c54da976767e342328167
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-09 22:47:51 +0900

    improve: change yaskkserv2 --google-japanese-input from last to notfound

--- nix-darwin/launchd/yaskkserv2.nix
+++ nix-darwin/launchd/yaskkserv2.nix
@@ -1,7 +1,7 @@
 { pkgs, profile, ... }:
 {
   launchd.user.agents.yaskkserv2 = {
-    command = "${pkgs.yaskkserv2}/bin/yaskkserv2 --google-suggest --google-japanese-input=last --google-cache-filename=/tmp/yaskkserv2.cache /Users/${profile.username}/.config/skk/yaskkserv2.dictionary";
+    command = "${pkgs.yaskkserv2}/bin/yaskkserv2 --google-suggest --google-japanese-input=notfound --google-cache-filename=/tmp/yaskkserv2.cache /Users/${profile.username}/.config/skk/yaskkserv2.dictionary";
     serviceConfig = {
       KeepAlive = true;
       RunAtLoad = true;
