bf32e80821e0a760f99ff3c81bf66bbc575279a6
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-11 22:49:06 +0900

    improve: modify to allow specifying configFile separately to easily extend cspell dictionary

--- modules/home-manager/tool/cspell.nix
+++ modules/home-manager/tool/cspell.nix
@@ -2,5 +2,7 @@
 {
   home.packages = with pkgs; [ nodePackages.cspell ];
 
-  xdg.configFile.cspell.source = ./configs/cspell;
+  xdg.configFile."cspell/cspell.json".source = ./configs/cspell/cspell.json;
+  xdg.configFile."cspell/user.txt".source = ./configs/cspell/user.txt;
+  xdg.configFile."cspell/vim.txt.gz".source = ./configs/cspell/vim.txt.gz;
 }
