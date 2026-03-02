0990f67876751ab9710203bac3f2b406cee134d4
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-03-02 22:13:45 +0900

    fix: fix issue where trusted-users are not configured correctly

--- modules/nix-darwin/nix/default.nix
+++ modules/nix-darwin/nix/default.nix
@@ -14,13 +14,11 @@
         "airrnot.cachix.org-1:w3kgp+iIESfezlrppODxo9Ahm1QLkCJXDhEuxYfUV7k="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      trusted-users = lib.mkDefault (
-        [
-          "root"
-          "@wheel"
-        ]
-        ++ lib.optional pkgs.stdenv.isDarwin "@admin"
-      );
+      trusted-users = [
+        "root"
+        "@wheel"
+      ]
+      ++ lib.optional pkgs.stdenv.isDarwin "@admin";
     };
   };
 }
