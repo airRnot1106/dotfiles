c7a67cfc0769dacbe3b0987a847bc296c759123f
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-03-24 22:48:39 +0900

    feat(lazygit): add Claude-powered commit message generator command

--- modules/home-manager/tool/lazygit.nix
+++ modules/home-manager/tool/lazygit.nix
@@ -10,6 +10,20 @@
         branchLogCmd = "git log --graph --color=always --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(#a0a0a0 reverse)%h%Creset %C(cyan)%ad%Creset %C(#dd4814)%ae%Creset %C(yellow reverse)%d%Creset %n%C(white bold)%s%Creset%n' {{branchName}} --";
         overrideGpg = true;
       };
+      customCommands = [
+        {
+          key = "<c-g>";
+          context = "files";
+          output = "terminal";
+          command = ''
+            MSG=$(git diff --cached | claude --no-session-persistence --print --model haiku \
+              'Generate ONLY a one-line Git commit message following Conventional Commits format \
+              (type(scope): description). Types: feat, fix, docs, style, refactor, test, chore. \
+              Based strictly on the diff from stdin. Output ONLY the message, nothing else.') \
+              && git commit -e -m "$MSG"
+          '';
+        }
+      ];
     };
   };
 }
