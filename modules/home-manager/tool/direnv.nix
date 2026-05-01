660cfd39fb00b23cce9dbb3894365fd9c60e8b69
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-05-01 10:45:01 +0900

    fix(direnv): disable checks on Darwin

--- modules/home-manager/tool/direnv.nix
+++ modules/home-manager/tool/direnv.nix
@@ -1,6 +1,10 @@
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
+    package = pkgs.direnv.overrideAttrs {
+      doCheck = !pkgs.stdenv.hostPlatform.isDarwin;
+    };
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
