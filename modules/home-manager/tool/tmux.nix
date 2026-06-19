2ec9dbecdc0781f916db6f327bce8834d21819ef
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-19 11:05:14 +0900

    feat(tool): add catppuccin theme to tmux

--- modules/home-manager/tool/tmux.nix
+++ modules/home-manager/tool/tmux.nix
@@ -1,5 +1,12 @@
+{ pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
+    plugins = [
+      {
+        plugin = pkgs.tmuxPlugins.catppuccin;
+        extraConfig = "set -g @catppuccin_flavour 'frappe'";
+      }
+    ];
   };
 }
