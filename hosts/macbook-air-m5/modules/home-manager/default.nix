bb41e5f79e2def121d23c9946c465cb681de51fb
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-19 00:24:35 +0900

    feat(host): scaffold macbook-air-m5 configuration

---
+++ hosts/macbook-air-m5/modules/home-manager/default.nix
@@ -0,0 +1,18 @@
+{
+  self,
+  inputs,
+  profile,
+  ...
+}:
+let
+  inherit (profile) username;
+in
+{
+  home-manager = {
+    extraSpecialArgs = { inherit self inputs profile; };
+    users.${username} = {
+      imports = [
+      ];
+    };
+  };
+}
---
+++ hosts/macbook-air-m5/profile.nix
@@ -0,0 +1,10 @@
+{
+  # general
+  system = "aarch64-darwin";
+  username = "airrnot";
+
+  # git
+  gitUsername = "airRnot1106";
+  gitEmail = "62370527+airRnot1106@users.noreply.github.com";
+  gitSigningKey = "B9F083E55A227726";
+}
