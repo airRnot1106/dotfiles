9f1f8df8ef70b852220389277e1b93a7abbf51b7
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-19 11:25:02 +0900

    feat: integrate neovim nightly overlay

---
+++ modules/home-manager/editor/neovim.nix
@@ -0,0 +1,15 @@
+{ inputs, pkgs, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
+    defaultEditor = true;
+    sideloadInitLua = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+  };
+}
