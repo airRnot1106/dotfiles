dbb2b4c558582aa7d72183f3b51094b0d045a1e1
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-07-19 15:44:04 +0900

    feat(nix-darwin): add nix configuration modules

---
+++ common/nix-darwin/nix/default.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, ... }:
+{
+  nix = {
+    optimise.automatic = lib.mkDefault true;
+    settings = {
+      experimental-features = lib.mkDefault "nix-command flakes";
+      extra-substituters = lib.mkDefault [
+        "https://airrnot.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://ryoppippi.cachix.org"
+      ];
+      extra-trusted-public-keys = lib.mkDefault [
+        "airrnot.cachix.org-1:w3kgp+iIESfezlrppODxo9Ahm1QLkCJXDhEuxYfUV7k="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
+      ];
+      max-jobs = lib.mkDefault 8;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ]
+      ++ lib.optional pkgs.stdenv.isDarwin "@admin";
+    };
+  };
+}
--- hosts/macbook-air-m5/modules/nix-darwin/default.nix
+++ hosts/macbook-air-m5/modules/nix-darwin/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./font
     ./homebrew
+    ./nix
   ];
 }
