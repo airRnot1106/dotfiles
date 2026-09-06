7f91c37b2a4e89f3df832bdcbf469d1b0c22f551
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-09-06 14:35:31 +0900

    chore(nix-darwin): add numtide cachix

--- common/nix-darwin/nix/default.nix
+++ common/nix-darwin/nix/default.nix
@@ -6,6 +6,7 @@
       experimental-features = lib.mkDefault "nix-command flakes";
       extra-substituters = lib.mkDefault [
         "https://airrnot.cachix.org"
+        "https://cache.numtide.com"
         "https://nix-community.cachix.org"
         "https://ryoppippi.cachix.org"
       ];
@@ -13,6 +14,7 @@
         "airrnot.cachix.org-1:w3kgp+iIESfezlrppODxo9Ahm1QLkCJXDhEuxYfUV7k="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "ryoppippi.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
       max-jobs = lib.mkDefault 8;
       trusted-users = [
